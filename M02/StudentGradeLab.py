4a59528e1232123191315c4531c6738fb80bf2ef
Jonathan Leverenz Desktop <jonlev03@gmail.com>
2023-03-28 16:50:21 -0400

    initial m02 commit

---
+++ M02/StudentGradeLab.py
@@ -0,0 +1,26 @@
+# Jonathan Leverenz
+# StudentGradeLab.py
+# This program will allow users to input their last name and grade to see if they made the dean list or honor roll. Program ends with a name of "ZZZ"
+
+# a loop that will continue until a break statement
+while (1):
+    # Get the student name and check for sentinal value
+    student_name = input("Please enter your last name : ")
+    if (student_name == "ZZZ"):
+        break
+
+    # get grade and check that it is an integer
+    grade = input("Please enter your grade : ")
+    try:
+        grade = float(grade)
+        if (grade >= 3.5):
+            print("You have made it on the dean list")
+
+        if (grade >= 3.25):
+            print("You have made Honor Roll")
+        else:
+            print("Maybe next time buddy")
+    except:
+        print("That was not a valid input, Please enter the next student...")
+
+print("Program ended")
